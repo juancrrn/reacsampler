c31aa26b192963547b97553285fdcacb8bacb326
Juan Carrión <hola@juancrrn.io>
2021-04-25 19:12:45 +0200

    Added comments

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * JavaScript general de la aplicación.
+ * 
+ * Usando el símbolo $ para variables de tipo objeto de jQuery.
+ *
+ * @package reacsampler
+ *
+ * @author juancrrn
+ *
+ * @version 0.0.1
+ */
